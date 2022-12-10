e1cc85ca54361f2dd1a56c57e351153413d23448
kanoi-y <furafuratonbo@gmail.com>
2022-12-10 23:11:28 +0900

    TweetListを作成

---
+++ src/components/TweetList/TweetList.tsx
@@ -0,0 +1,7 @@
+import type { FC } from "react";
+
+type Props = {};
+
+export const TweetList: FC<Props> = () => {
+  return <div></div>;
+};
---
+++ src/components/TweetList/index.ts
@@ -0,0 +1,1 @@
+export { TweetList } from "./TweetList";
