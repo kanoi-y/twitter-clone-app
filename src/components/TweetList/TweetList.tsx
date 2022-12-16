bb4526219682fccdeed915198009bfcdb5da994f
kanoi-y <furafuratonbo@gmail.com>
2022-12-16 11:05:29 +0900

    ツイートのコメントとイイネをpropsから受け取るように実装

--- src/components/TweetList/TweetList.tsx
+++ src/components/TweetList/TweetList.tsx
@@ -1,4 +1,4 @@
-import type { Tweet, User } from "@prisma/client";
+import type { Comment, Favorite, Tweet, User } from "@prisma/client";
 import { formatDistanceToNowStrict } from "date-fns";
 import { ja } from "date-fns/locale";
 import Image from "next/image";
@@ -7,9 +7,16 @@ import type { FC } from "react";
 type Props = {
   tweet: Tweet;
   createdUser: Omit<User, "email" | "emailVerified">;
+  comments: Comment[];
+  favorites: Favorite[];
 };
 
-export const TweetList: FC<Props> = ({ tweet, createdUser }) => {
+export const TweetList: FC<Props> = ({
+  tweet,
+  createdUser,
+  comments,
+  favorites,
+}) => {
   return (
     <div className="flex cursor-pointer items-start border-b border-[rgb(239,243,244)] px-4 py-3 hover:bg-slate-50">
       {createdUser.image ? (
@@ -56,7 +63,9 @@ export const TweetList: FC<Props> = ({ tweet, createdUser }) => {
                   d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
                 />
               </svg>
-              <span className="block px-3 text-sm text-current">2</span>
+              <span className="block px-3 text-sm text-current">
+                {comments.length || ""}
+              </span>
             </button>
           </div>
           <div className="flex-1">
@@ -75,7 +84,9 @@ export const TweetList: FC<Props> = ({ tweet, createdUser }) => {
                   d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
                 />
               </svg>
-              <span className="px-3 text-sm text-current">2</span>
+              <span className="px-3 text-sm text-current">
+                {favorites.length || ""}
+              </span>
             </button>
           </div>
         </div>
