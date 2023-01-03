54ea8013da4d940f23e5d1e6596e967ad442d4b6
kanoi-y <furafuratonbo@gmail.com>
2023-01-03 22:39:41 +0900

    ✨ tweetを作成できるように実装

--- src/components/CreateTweet/CreateTweet.tsx
+++ src/components/CreateTweet/CreateTweet.tsx
@@ -4,14 +4,27 @@ import Link from "next/link";
 import type { FC } from "react";
 import { useState } from "react";
 import { useAutoResizeTextArea } from "~/hooks";
+import { trpc } from "~/utils/trpc";
 
 type Props = {
   currentUser: User;
 };
 
 export const CreateTweet: FC<Props> = ({ currentUser }) => {
+  const utils = trpc.useContext();
+
   const [tweetText, setTweetText] = useState("");
   const textAreaRef = useAutoResizeTextArea(tweetText);
+  const createTweet = trpc.tweet.createTweet.useMutation({
+    async onSuccess() {
+      await utils.tweet.getTweets.invalidate();
+    },
+  });
+
+  const handleClickTweetBtn = async () => {
+    await createTweet.mutateAsync({ text: tweetText });
+    setTweetText("");
+  };
 
   return (
     <div className="flex items-start">
@@ -47,7 +60,11 @@ export const CreateTweet: FC<Props> = ({ currentUser }) => {
           />
         </div>
         <div className="flex justify-end pt-3">
-          <button className="btn-primary btn rounded-full border-none text-base text-white">
+          <button
+            className="btn-primary btn rounded-full border-none text-base text-white disabled:bg-primary disabled:text-white disabled:opacity-60"
+            disabled={tweetText.trim().length === 0}
+            onClick={handleClickTweetBtn}
+          >
             ツイートする
           </button>
         </div>
