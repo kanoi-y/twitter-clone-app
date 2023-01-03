bdb1c2d172a84ef1d3e9c3678230b5c58a0e5636
kanoi-y <furafuratonbo@gmail.com>
2023-01-03 22:39:12 +0900

    🎨tailwind.config.cjsを修正

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(29,155,240)",
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
     themes: [
       {
         mytheme: {
-          "primary": "rgb(29,155,240)",
+          primary: "rgb(29,155,240)",
         },
       },
     ],
