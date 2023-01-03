3fcc05dddf643ed9482a1bd573513fac8ea9c562
kanoi-y <furafuratonbo@gmail.com>
2023-01-03 22:10:29 +0900

    ✨ textareaの高さをコンテンツ量で調整されるように実装

--- src/components/CreateTweet/CreateTweet.tsx
+++ src/components/CreateTweet/CreateTweet.tsx
@@ -2,12 +2,17 @@ import type { User } from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 import type { FC } from "react";
+import { useState } from "react";
+import { useAutoResizeTextArea } from "~/hooks";
 
 type Props = {
   currentUser: User;
 };
 
 export const CreateTweet: FC<Props> = ({ currentUser }) => {
+  const [tweetText, setTweetText] = useState("");
+  const textAreaRef = useAutoResizeTextArea(tweetText);
+
   return (
     <div className="flex items-start">
       {currentUser.image ? (
@@ -34,12 +39,15 @@ export const CreateTweet: FC<Props> = ({ currentUser }) => {
       <div className="flex-1">
         <div className="border-b border-[rgb(239,243,244)]">
           <textarea
-            className="textarea w-full resize-none text-xl text-[rgb(15,20,25)] focus:outline-none active:outline-none"
+            className="textarea w-full resize-none overflow-hidden text-xl text-[rgb(15,20,25)] focus:outline-none active:outline-none"
             placeholder="いまどうしてる？"
-          ></textarea>
+            value={tweetText}
+            onChange={(e) => setTweetText(e.target.value)}
+            ref={textAreaRef}
+          />
         </div>
         <div className="flex justify-end pt-3">
-          <button className="btn-primary btn rounded-full border-none text-white text-base">
+          <button className="btn-primary btn rounded-full border-none text-base text-white">
             ツイートする
           </button>
         </div>
